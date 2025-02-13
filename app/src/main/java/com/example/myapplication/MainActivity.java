9127ac40aeb09ae6bc50daa993fd5a79d358df05
Santosh Neupane <sanneupane1024@gmail.com>
2025-02-13 21:50:38 +0545

    add button event code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,22 +13,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //- enable edge-to-edge display
-        //- could be excluded
-//        EdgeToEdge.enable(this);
-
         setContentView(R.layout.login);
 
-        //- used to handle window insets in an Android app
-        //- could be excluded
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        // method 1: on register button
+        Button myButton = findViewById(R.id.registerBtn);
+        myButton.setOnClickListener(new RegisterClickListener());
 
-        //- Insets represent the areas of the screen occupied by system UI
-        //- elements like the status bar, navigation bar, and gesture insets.
+        // method 2 : on login button
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Login Button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private class RegisterClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Register Button Clicked!", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
