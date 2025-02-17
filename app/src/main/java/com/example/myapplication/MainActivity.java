9c5ffe23b8f3c74378f8e06cdce8ded76ac1e8aa
Santosh Neupane <sanneupane1024@gmail.com>
2025-02-17 18:54:02 +0545

    updates

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         //- could be excluded
         EdgeToEdge.enable(this);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear);
 
         //- used to handle window insets in an Android app
         //- could be excluded
@@ -30,5 +34,41 @@ public class MainActivity extends AppCompatActivity {
 
         //- Insets represent the areas of the screen occupied by system UI
         //- elements like the status bar, navigation bar, and gesture insets.
+
+        Button addButton = findViewById(R.id.buttonAdd);
+        Button multiplyButton = findViewById(R.id.buttonMultiply);
+        EditText firstNumberEditText = findViewById(R.id.firstNumET);
+        EditText secondNumberEditText = findViewById(R.id.secondNumET);
+        TextView resultTv = findViewById(R.id.resultTV);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstNumStr = firstNumberEditText.getText().toString();
+                Double firstNum = Double.parseDouble(firstNumStr);
+
+                String secondNumStr = secondNumberEditText.getText().toString();
+                Double secondNum = Double.parseDouble(secondNumStr);
+
+                Double result = firstNum + secondNum;
+
+                resultTv.setText(result.toString());
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstNumStr = firstNumberEditText.getText().toString();
+                Double firstNum = Double.parseDouble(firstNumStr);
+
+                String secondNumStr = secondNumberEditText.getText().toString();
+                Double secondNum = Double.parseDouble(secondNumStr);
+
+                Double result = firstNum * secondNum;
+
+                resultTv.setText(result.toString());
+            }
+        });
     }
 }
