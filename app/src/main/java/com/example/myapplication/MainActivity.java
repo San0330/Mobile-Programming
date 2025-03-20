2674bd4a0f99993e372a756e9bbef8b8de46ec24
Santosh Neupane <sanneupane1024@gmail.com>
2025-03-20 07:51:42 +0545

    update custom dialog

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -30,5 +38,42 @@ public class MainActivity extends AppCompatActivity {
 
         //- Insets represent the areas of the screen occupied by system UI
         //- elements like the status bar, navigation bar, and gesture insets.
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+    
+    public void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Calculate Sum");
+        builder.setCancelable(true);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.custom_dialog, null);
+        builder.setView(view);
+
+        EditText edtFirst = view.findViewById(R.id.edtFirst);
+        EditText edtSecond = view.findViewById(R.id.edtSecond);
+        Button btnCalculate = view.findViewById(R.id.btnCalculate);
+        TextView txtResult = view.findViewById(R.id.txtResult);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int first, second, result;
+                first  = Integer.parseInt(edtFirst.getText().toString());
+                second = Integer.parseInt(edtSecond.getText().toString());
+                result = first + second;
+                txtResult.setText("Result : " + result);
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
